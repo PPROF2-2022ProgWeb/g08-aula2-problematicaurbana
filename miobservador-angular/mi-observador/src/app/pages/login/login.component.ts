17e1f5a2125dc61417350c48cf6e44cddade7746
MaryselR <69443638+MaryselR@users.noreply.github.com>
2022-11-14 23:43:38 -0300

    Recuperacion de carpeta Angular

--- miobservador-angular/mi-observador/src/app/pages/login/login.component.ts
+++ miobservador-angular/mi-observador/src/app/pages/login/login.component.ts
@@ -1,42 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import { UserFields } from '../Usuario';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+ 
   
-  form!: FormGroup
-
-  constructor() { 
-    this.buildForm();
-  }
-
-  ngOnInit() {
-  }
-
-  private buildForm() {
-    this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      contrasenia: new FormControl('',  [Validators.maxLength(8)])
-        });
-  
-        this.form.valueChanges
-        .subscribe(value => {
-          console.log(value);
-        });
-      }
     
-      
-      save(event: Event) {
-        event.preventDefault();
-        if (this.form.valid) {
-          const value = this.form.value;
-          console.log(value);
-        } else {
-          this.form.markAllAsTouched();
-        }
+  UsuarioForm!: FormGroup
+  resultado!: string;
+  submitted = false;
+
+ 
+    isDone = true;
+ 
+
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+    
+  createForm() {
+    this.UsuarioForm = this.fb.group({
+        
+      email:       new FormControl('', [Validators.required,
+                                        Validators.email,
+                                       ]),
+      password: new FormControl('',    [Validators.maxLength(8),
+                                        Validators.minLength(8),
+                                        Validators.required])
+                                        });
+                                      
+                                     
+    this.UsuarioForm.controls["email"].valueChanges.subscribe(data => {
+                              console.log(data);
+                                        });
+    this.UsuarioForm.controls["password"].valueChanges.subscribe(data => {
+                                          console.log(data);
+                                        });
+              }
+                     
+  onSubmit() {
+    this.submitted = true;
+     if (this.UsuarioForm.invalid) {
+      return;
       }
+        }
+                                      
+get email() { return this.UsuarioForm.get('email'); }
+
+get password() { return this.UsuarioForm.get('password'); } 
+                                        
+get f() {return this.UsuarioForm.controls; }
+
+ngOnInit() {}                               
+  
+    
+
+    
     }
+    
